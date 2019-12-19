7f893bb67367e84142d959606284fa8d86fe90c6
cultcreep <patrickgkelly13@gmail.com>
2019-12-19 12:08:18 -0500

    Add existing file

---
+++ app.py
@@ -0,0 +1,6 @@
+Character_name = "John"
+Character_age = "35"
+print("There once was a man named " + Character_name + ", ")
+print("He was " + Character_age + " years old. ")
+print("He really liked the name "+ Character_name +",")
+print("but he didn't like being " + Character_age +",  ")
